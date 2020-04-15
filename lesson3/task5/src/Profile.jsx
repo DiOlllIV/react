668002695383e7f73be1f6e822b34791ac675125
Denis Olekh <denisolekh@gmail.com>
2020-04-15 15:00:03 +0300

    hw

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
+import UserInfo from './UserInfo';
 
-const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-const getDateBirth = date => {
-    const day = new Date(date).getDate();
-    const year = new Date(date).getYear();
-    const month = monthList[new Date(date).getMonth()];
-
-    return `${day} ${month} ${year}`;
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
 };
 
-const Profile = (props) => {
+const Profile = () => {
     return (
         <div className="profile">
-            <span className="profile__name">
-                {`${props.userData.firstName} ${props.userData.lastName}.`}
-            </span>
-            <span className="profile__birth">
-                {`Was born ${getDateBirth(props.userData.birthDate)}
-                    in ${props.userData.birthPlace}.`}
-            </span>
+            <UserInfo userData={user} />
         </div>
     );
 };
--- lesson3/task5/src/index.jsx
+++ lesson3/task5/src/index.jsx
@@ -5,11 +5,4 @@ import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    birthDate: '1991-01-17T11:11:11.819Z',
-    birthPlace: 'London'
-};
-
-ReactDOM.render(<Profile userData={user} />, rootElem);
+ReactDOM.render(<Profile />, rootElem);
