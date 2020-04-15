09ece62fa5d9d24ae9597496a77170e710afeb41
Denis Olekh <denisolekh@gmail.com>
2020-04-15 14:06:44 +0300

    hw

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Comment from './Comment';
 
 const rootElem = document.querySelector('#root');
 
+const userInfo ={
+    name: 'Tom',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u10001'
+  }
+
 ReactDOM.render(
-    <App />,
+    <Comment
+      user={userInfo}
+      text="Good job"
+      date={new Date()}
+    />,
     rootElem
 );
