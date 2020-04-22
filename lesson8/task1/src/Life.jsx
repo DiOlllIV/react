4b301e432c3589d5e51a5d74cf917477a2862f40
Denis Olekh <denisolekh@gmail.com>
2020-04-22 17:58:43 +0300

    hw

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -10,11 +10,12 @@ class Life extends Component {
         console.log('componentDidMount: API calls, subscriptions');
     }
 
-    shouldComponentUpdate(){
+    shouldComponentUpdate(nextProps, nextState){
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+        return this.props !== nextProps;
     }
 
-    componentDidUpdate() {
+    componentDidUpdate(prevProps, prevState) {
         console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
 
