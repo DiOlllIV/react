2f1d3457fcccd50190598cfd45e7d65fd790ec9c
Denis Olekh <denisolekh@gmail.com>
2020-04-23 17:02:09 +0300

    hw

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -16,7 +16,7 @@ class Search extends Component {
 
     search = e => {
         e.preventDefault();
-        alert(this.state.value);
+        alert(`Search text: ${this.state.value}`);
     }
 
     render() {
