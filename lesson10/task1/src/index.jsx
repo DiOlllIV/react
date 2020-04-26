536700b0411d0f397f302e058eac4e6605a30653
Denis Olekh <denisolekh@gmail.com>
2020-04-26 13:05:14 +0300

    hw

--- lesson10/task1/src/index.jsx
+++ lesson10/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
+import App from './App';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<Page userId="sonywws"/>, rootElem);
+ReactDOM.render(<App userId="sonywws"/>, rootElem);
