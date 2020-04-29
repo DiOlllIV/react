b5772c55dc78f207ba85b82b4a851381278fca46
Denis Olekh <denisolekh@gmail.com>
2020-04-29 15:18:00 +0300

    hw

--- lesson11/task3/src/index.jsx
+++ lesson11/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  from './';
+import App from './App';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(< />, rootElem);
+ReactDOM.render(<App />, rootElem);
