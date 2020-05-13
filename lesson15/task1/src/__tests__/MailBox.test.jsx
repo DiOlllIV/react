cdc2422c32223f593b0a5dec5d3e1b806a1c60cf
Denis Olekh <denisolekh@gmail.com>
2020-05-13 18:22:41 +0300

    hw

--- lesson15/task1/src/__tests__/MailBox.test.jsx
+++ lesson15/task1/src/__tests__/MailBox.test.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-import MailBox from '../MailBox'
+import Mailbox from '../Mailbox'
 
 describe('<Mailbox/>', () => {
   it('should return 2 unreadmessages', () => {
     const unreadMessages = [1, 2];
-    const wrappedComponent = shallow(<MailBox unreadMessages={unreadMessages} />);
+    const wrappedComponent = shallow(<Mailbox unreadMessages={unreadMessages} />);
 
     expect(wrappedComponent).toMatchSnapshot();
   });
 });
 
 describe('<Mailbox/>', () => {
-  it('should return only MailBox without undereadmessages', () => {
+  it('should return only Mailbox without undereadmessages', () => {
     const unreadMessages = [];
-    const wrappedComponent = shallow(<MailBox unreadMessages={unreadMessages}/>);
+    const wrappedComponent = shallow(<Mailbox unreadMessages={unreadMessages}/>);
 
     expect(wrappedComponent).toMatchSnapshot();
   });
